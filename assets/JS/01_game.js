9ab8eb617bfaea89b6dc57cc8a7c2a7d29f7a984
Ryan <ryanspooner92@gmail.com>
2018-06-21 10:24:47 -0700

    developed game: Breakout

--- assets/JS/01_game.js
+++ assets/JS/01_game.js
@@ -1,18 +1,47 @@
+//
+// Code for the game: BREAKOUT
+// ____________________________
+//
+
+// Get reference to the canvas
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
+// Ball attributes
 var x = canvas.width/2;
 var y = canvas.height-30;
-
 var dx = 2;
 var dy = -2;
 var ballRadius = 10;
 
+// Paddle attributes
 var paddleHeight = 10;
 var paddleWidth = 75;
 var paddleX = (canvas.width-paddleWidth)/2;
 var rightPressed = false;
 var leftPressed = false;
+
+// Detect user input
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+    // set boolean to move the paddle if a key is pressed
+function keyDownHandler(e) {
+  if (e.keyCode == 39) {
+    rightPressed = true;
+  }
+  else if (e.keyCode == 37) {
+    leftPressed = true;
+  }
+}
+    // set boolean to not move is key is released or not pressed
+function keyUpHandler(e) {
+  if (e.keyCode == 39) {
+    rightPressed = false;
+  }
+  else if (e.keyCode == 37) {
+    leftPressed = false;
+  }
+}
 
 function drawBall() {
   ctx.beginPath();
@@ -34,45 +63,40 @@ function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
   drawPaddle();
-  x += dx;
-  y += dy;
 
-  // Detect collisions with top and bottom of screen
-  if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+  // Detect ball collisions with top, bottom, and sides of the canvas
+  // Hitting the bottom results in a game over
+  //
+      // Let and Right collisions
+  if (x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
     dx = -dx;
   }
-  if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+      // Top collision
+  if (y + dy < ballRadius) {
     dy = -dy;
   }
+      // Bottom collision. If the ball hits the paddle the games goes on
+  else if (y + dy > canvas.height-ballRadius) {
+    if (x > paddleX && x < paddleX + paddleWidth) {
+      dy = -dy;
+    }
+    else {
+      alert("GAME OVER");
+      clearInterval(game);
+      document.location.reload();
+    }
+  }
 
-  // Move the paddle
-  if(rightPressed && paddleX < canvas.width-paddleWidth) {
+  // Move the paddle based on user input
+  if (rightPressed && paddleX < canvas.width-paddleWidth) {
     paddleX += 7;
   }
   else if (leftPressed && paddleX > 0) {
     paddleX -= 7;
   }
+
+  x += dx;
+  y += dy;
 }
-
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
-
-function keyDownHandler(e) {
-  if (e.keyCode == 39) {
-    rightPressed = true;
-  }
-  else if (e.keyCode == 37) {
-    leftPressed = true;
-  }
-}
-
-function keyUpHandler(e) {
-  if (e.keyCode == 39) {
-    rightPressed = false;
-  }
-  else if (e.keyCode == 37) {
-    leftPressed = false;
-  }
-}
-
-setInterval(draw, 10);
+// draw the shapes every 10ms
+var game = setInterval(draw, 10);
